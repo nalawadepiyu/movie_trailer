fd2eaad7ba7fc3e16b6086b3ca36df92acb6d28b
Priyanka Nalawade <piyu1081991@gmail.com>
2016-12-17 22:41:05 -0800

    Init: Initialize repository

---
+++ media.py
@@ -0,0 +1,9 @@
+class Movie():
+    def __init__(self,movie_title,movie_storyline,movie_poster_image,movie_youtube_url):
+        self.title = movie_title
+        self.storyline = movie_storyline
+        self.poster_image_url = movie_poster_image
+        self.trailer_youtube_url = movie_youtube_url
+
+
+    
